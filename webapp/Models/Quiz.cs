b229bf625a1bf93051a342c05478188eb86433e2
au601009 <201808572@post.au.dk>
2023-04-02 16:07:50 +0200

    update

---
+++ webapp/Models/Quiz.cs
@@ -0,0 +1,13 @@
+﻿namespace webapp.Models
+{
+    public class Quiz
+    {
+        public int Id { get; set; }
+        public string QuizQuestion { get; set; }
+        public string QuizAnswer { get; set; }
+        public Quiz()
+        {
+            
+        }
+    }
+}
